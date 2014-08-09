dbbecb8c5c41686e122b5379f69250a5e20d226c
John Jacobsen <john@mail.npxdesigns.com>
2014-08-09 12:12:37 -0500

    Mainify things.  Fixes #1.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject arx "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject arx "0.0.0"
+  :description "Experiments in 3d design."
+  :url "https://github.com/eigenhombre/arx/"
+  :main arx.core
+  :aot [arx.core]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
