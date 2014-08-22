24561047de3dcbe3ecac456a455243804646c227
John Jacobsen <john@mail.npxdesigns.com>
2014-08-21 21:30:25 -0500

    Fixes #12.  Fixes #13.  Fixes #14.  Fixes #15.

--- src/arx/core.clj
+++ src/arx/core.clj
@@ -5,9 +5,13 @@
 
 
 (defn setup []
-  (q/set-state! :phi 0
-                :r 1000
-                :theta (q/radians 45)
+  (q/set-state! :t 0
+                :phi 0
+                :target-phi 0
+                :r 4000
+                :target-r 4000
+                :theta (q/radians 75)
+                :target-theta (q/radians 75)
                 :paused false))
 
 
@@ -23,8 +27,28 @@
 (defn theta [] (:theta (q/state)))
 (defn r [] (:r (q/state)))
 
-(defn update-rotation []
-  (swap! (q/state-atom) update-in [:phi] (partial + (q/radians 0.5))))
+
+(defn update [kw f]
+  (swap! (q/state-atom) update-in [kw] f))
+
+
+(defn assign [kw val]
+  (swap! (q/state-atom) assoc-in [kw] val))
+
+
+(defn getk [kw] (get @(q/state-atom) kw))
+
+
+(defn update-camera []
+  (update :phi (partial + (q/radians 0.10)))
+  (update :t inc)
+  (assign :target-r (+ 4000 (* 2000 (Math/sin (* 0.001 (getk :t))))))
+  (assign :target-theta (+ (q/radians 65) (* (q/radians 10)
+                                             (Math/sin (* 0.005 (getk :t))))))
+  (let [del-theta (* 0.01 (- (getk :target-theta) (getk :theta)))
+        del-r (* 0.005 (- (getk :target-r) (getk :r)))]
+    (swap! (q/state-atom) update-in [:theta] (partial + del-theta))
+    (swap! (q/state-atom) update-in [:r] (partial + del-r))))
 
 
 (defn key-press []
@@ -47,33 +71,56 @@
     (q/stroke 0)))
 
 
-(defn add-random-box []
-  (let [width 1000
-        r (* (Math/log (- 1 (q/random 1))) (- width))
+(defn add-box [width min-base max-base min-height max-height]
+  (let [r (* (Math/log (- 1 (q/random 1))) (- width))
         phi (q/random q/TWO-PI)
         x (* r (Math/cos phi))
         y (* r (Math/sin phi))
         zeta (q/random q/TWO-PI)
-        size (q/random 200)]
-    (swap! boxes conj [x y size zeta])))
+        base (q/random min-base max-base)
+        height (q/random min-height max-height)]
+    (swap! boxes conj [x y base height zeta])))
+
+
+(defn add-wide-box []
+  (add-box 1000 0 400 0 100))
+
+
+(defn add-narrow-box []
+  (add-box 4000 100 150 600 700))
+
+
+(defn add-either-box []
+  (if (< (q/random 1) 0.04)
+    (add-narrow-box)
+    (add-wide-box)))
 
 
 (defn draw-boxes []
-  (doseq [[x y l zeta] @boxes]
+  (doseq [[x y base height zeta] @boxes]
     (q/push-matrix)
-    (q/translate x y (/ l 2))
+    (q/translate x y (/ height 2))
     (q/rotate-z zeta)
-    (q/box l)
+    (q/box base base height)
     (q/pop-matrix)))
 
 
+(defn update-perspective []
+  (q/perspective q/THIRD-PI
+                 (/ (q/width) (q/height))
+                 (/ (:r (q/state)) 10.0)
+                 (* (:r (q/state)) 500.0)))
+
 (defn draw []
+  (q/lights)
   (q/background 200)
   (draw-axes)
   (when-not (paused)
-    (update-rotation)
-    (when (< (q/random 1) 0.1)
-      (add-random-box)))
+    (update-camera)
+    (update-perspective)
+    (while (or (< (count @boxes) 1000)
+               (< (q/random 1) 0.03))
+      (add-either-box)))
   (q/camera (* (r) (Math/cos (phi)) (Math/sin (theta)))
             (* (r) (Math/sin (phi)) (Math/sin (theta)))
             (* (r) (Math/cos (theta)))
@@ -95,10 +142,7 @@
 
 (defn mouse-wheel [amount]
   (swap! (q/state-atom) update-in [:r] #(max 1 (+ % (* 7 amount))))
-  (q/perspective q/THIRD-PI
-                 (/ (q/width) (q/height))
-                 (/ (:r (q/state)) 10.0)
-                 (* (:r (q/state)) 100.0)))
+  (update-perspective))
 
 
 (defn -main []
