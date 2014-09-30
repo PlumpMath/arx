5495f8a046f0f90e117258b75b5132725bf36f35
John Jacobsen <john@mail.npxdesigns.com>
2014-09-29 22:28:34 -0500

    Tests for new representation. Fixes #22.

--- test/arx/geom_test.clj
+++ test/arx/geom_test.clj
@@ -12,6 +12,7 @@
 (defn all-face-paths [f] f)
 (defn face-boundary [f] (first f))
 
+
 (defn- extrude-direction [f]
   (let [[[e00 e01] [_ e11]] (face-boundary f)
         v1 (o/- e01 e00)
@@ -19,18 +20,16 @@
     (m/normalise (m/cross v2 v1))))
 
 
-(defn- extrude [face amt]
-  (let [paths (all-face-paths face)
-        points []
-        dir (extrude-direction points)
+(defn- extrude [f amt]
+  (let [dir (map #(Math/abs %) (extrude-direction f))
         vdir (o/* dir amt)]
-    (concat
-     (for [[p1 p2]
-           (take (count points)
-                 (partition 2 1 (cycle points)))]
-       [p1 p2 (o/+ vdir p2) (o/+ vdir p1)])
-     [points
-      (map (partial o/+ vdir) points)])))
+    (conj (for [pth (all-face-paths f)
+                [p1 p2] pth]
+            (face (path (edge p1 p2)
+                        (edge p2 (o/+ vdir p2))
+                        (edge (o/+ vdir p2) (o/+ vdir p1))
+                        (edge (o/+ vdir p1) p1))))
+          f f)))
 
 
 (fact                               "I can write long and interesting
@@ -94,50 +93,22 @@
 
     (fact                           "I can get the correct normal to a
                                      horizontal face."
-      (->> p1
-           face
+      (->> (face p1)
            extrude-direction
            (map #(Math/abs %))) => [0.0 0.0 1.0])
 
     (fact                           "I can extrude a face, turning all
                                      edges into faces and adding a
-                                     'top' face."
-      ;; (-> p1
-      ;;     face
-      ;;     (extrude 3))
-      )))
+                                     'top' face to the side and bottom faces."
+      (-> (face p1)
+          (extrude 3)
+          count) => 5)
 
+    (fact                           "I can extrude a face with a hole and
+                                     get the right number of faces."
+      (-> (face p1 p2)
+          (extrude 3)
+          count) => 8)
 
-
-
-;; WIP: finish adapting these to point/edge/path/face model:
-
-
-(defn- split-edge [[p1 p2] amt]
-  (let [del (o/* (o/- p2 p1) amt)]
-    [(o/+ p1 del)
-     (o/- p2 del)]))
-
-
-;; #_((defn- split-rect [face edge-index-0 edge-index-1 amt0 amt1])
-;;  [[0 0 0] [1 0 0] [1 1 0] [0 1 0]])
-
-
-(comment
-  (let [base [[0 0 0]
-              [0 1000 0]
-              [1000 1000 0]
-              [1000 0 0]]
-        cube (extrude base 1000)]
-    cube)
-
-  ;;=>
-  [[[0 0 0] [0 1000 0] [0.0 1000.0 1000.0] [0.0 0.0 1000.0]]
-   [[0 1000 0] [1000 1000 0] [1000.0 1000.0 1000.0] [0.0 1000.0 1000.0]]
-   [[1000 1000 0] [1000 0 0] [1000.0 0.0 1000.0] [1000.0 1000.0 1000.0]]
-   [[1000 0 0] [0 0 0] [0.0 0.0 1000.0] [1000.0 0.0 1000.0]]
-   [[0 0 0] [0 1000 0] [1000 1000 0] [1000 0 0]]
-   [[0.0 0.0 1000.0]
-    [0.0 1000.0 1000.0]
-    [1000.0 1000.0 1000.0]
-    [1000.0 0.0 1000.0]]])
+    (future-fact                    "Top and bottom faces after extraction
+                                     all have the right z coordinate.")))
